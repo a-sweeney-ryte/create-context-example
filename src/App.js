9a783d997b6437e8be41f81907033fde7eeca7c4
Alex Sweeney <a.sweeney@ryte.com>
2022-04-08 17:35:19 +0100

    add functional GrandChildComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const ThemeContext = React.createContext();
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeContextProvider value={{ theme, setTheme }}>
+      <ChildComponent/>
+    </ThemeContextProvider>
+  )
+}
+
+function ChildComponent() {
+  return <GrandChildComponent/>
+}
+
+// class method
+/*
+class GrandChildComponent {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {({ theme, setTheme })} => {
+          return (
+            <>
+              <div>The theme is {theme}</div>
+              <button onClick={() => setTheme('light')}>
+                Change To Light Theme
+              </button>
+            </>
+          )
+        }
+      </ThemeContext.Consumer>
+    )
+  }
+}
+*/
+
+function GrandChildComponent() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <>
+      <div>The theme is {theme}</div>
+      <button onClick={() => setTheme('light')}>
+        Change To Light Theme
+      </button>
+    </>
+  )
 }
 
 export default App;
